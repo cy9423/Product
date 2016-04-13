4198499aa4b22630e45639d239fc039f3bd5681c
com.obam <18647095504@163.com>
2016-04-13 13:46:08 +0800

    test

---
+++ Product/Class/Shop/View/ShopScrollView.h
@@ -0,0 +1,13 @@
+//
+//  ShopScrollView.h
+//  Product
+//
+//  Created by cy on 16/4/13.
+//  Copyright © 2016年 cy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShopScrollView : UIView
+
+@end
---
+++ Product/Class/Shop/View/ShopScrollView.m
@@ -0,0 +1,21 @@
+//
+//  ShopScrollView.m
+//  Product
+//
+//  Created by cy on 16/4/13.
+//  Copyright © 2016年 cy. All rights reserved.
+//
+
+#import "ShopScrollView.h"
+
+@implementation ShopScrollView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
